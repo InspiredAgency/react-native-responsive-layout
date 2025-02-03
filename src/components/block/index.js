39573959ecd5bebc0908b78b0a47317db8d9dc47
Agon Selimi <selimi.agon97@gmail.com>
2025-02-03 10:59:58 +0100

    fix block warning

--- src/components/block/index.js
+++ src/components/block/index.js
@@ -3,54 +3,45 @@ import PropTypes from "prop-types";
 import { StyleSheet, View } from "react-native";
 
 import { SIZE_NAMES, GRID_UNITS, VERTICAL } from "../../shared/constants";
-
 import { roundForPercentage } from "../../shared/methods";
 import { ContainerSizeProp, DirectionProp } from "../../shared/props";
-import { checkInsideGrid } from "../../utils";
 import { determineSize, isHidden } from "./methods";
 import BlockProps from "./props";
 import { GridContext } from "../grid";
 
 // We need to ensure that stretch sizing wouldn't collapse to zero width when
-// there is enough elements to already fill the line.
+// there are enough elements to already fill the line.
 const ONE_UNIT_WIDTH = `${roundForPercentage(100 / GRID_UNITS)}%`;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   stretchSize: {
     flex: 1,
     flexBasis: ONE_UNIT_WIDTH,
   },
 });
 
-/* eslint-disable */
 /**
- * Element representing single cell in grid structure. It only works when nested
- * inside Grid component.
+ * Element representing a single cell in a grid structure.
  *
- * It only accepts size classes as arguments:
- * - if there are none provided it will fallback to full width
- * - if there is only `size` provided it will be same on all layouts
- * - otherwise it will choose smallest class defined that is applicable based
- *   on sizing of closest outer `Grid` element
- *
- * @type {React.StatelessComponent<{size?: string | number, hidden?: boolean, visible?: boolean, style?: any, children: any}>}
+ * @type {React.FC<{size?: string | number, hidden?: boolean, visible?: boolean, style?: any, children?: any}>}
  */
-/* eslint-enable */
 const Block = ({ children, ...props }) => {
+  const { gridContentDirection, gridSizeClass } = useContext(GridContext);
+
+  // Ensure `isHidden` check happens after context is retrieved
   if (isHidden(SIZE_NAMES, gridSizeClass, props)) {
     return null;
   }
-  const { gridContentDirection, gridSizeClass } = useContext(GridContext);
 
-  // Which attribute we set depends on direction
+  // Determine the style property based on grid direction
   const styleProperty = gridContentDirection === VERTICAL ? "width" : "height";
 
   // Determine size
   const size = determineSize(SIZE_NAMES, gridSizeClass, props);
-  const constantSize = { [styleProperty]: size };
-  const sizeStyle = size === "stretch" ? style.stretchSize : constantSize;
+  const sizeStyle =
+    size === "stretch" ? styles.stretchSize : { [styleProperty]: size };
 
-  // flexDirection depends on direction
+  // Define flex direction based on grid direction
   const directionStyle = {
     flexDirection: gridContentDirection === VERTICAL ? "column" : "row",
   };
@@ -60,21 +51,16 @@ const Block = ({ children, ...props }) => {
   );
 };
 
-Block.defaultProps = {
-  children: null,
-};
-
-Block.contextTypes = {
-  gridSizeClass: checkInsideGrid(ContainerSizeProp),
-  gridContentDirection: checkInsideGrid(DirectionProp),
-};
-
 Block.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
   ...BlockProps,
+};
+
+Block.defaultProps = {
+  children: null,
 };
 
 export default Block;
